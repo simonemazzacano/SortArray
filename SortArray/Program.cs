08d6c7e64351db3b82ec436e9cfe1e22317cd33f
Simone Mazzacano <SIMONE.MAZZACANO@local.ittcesena.it>
2021-10-08 08:16:26 +0200

    Prima versione

---
+++ SortArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 10, 2, 9, 34, -2, 4, 20 };
+            Array.Sort(arr);
+            foreach (int val in arr)
+            {
+                Console.Write(val + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
